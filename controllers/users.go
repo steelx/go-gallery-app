f4c0f355970e4a6dc3dd74ae51b04aed0d2d68b4
Ajinkya Borade <ajinkyab1987@gmail.com>
2019-03-27 20:41:30 +0530

    Create user with SignUpForm struct schema

--- controllers/users.go
+++ controllers/users.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"fmt"
+	"github.com/gorilla/schema"
 	"go-gallery-app/views"
 	"net/http"
 )
@@ -16,6 +17,11 @@ type Users struct {
 	NewView *views.View
 }
 
+type SignUpForm struct {
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
+}
+
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	err := u.NewView.Render(w, nil)
 	if err != nil {
@@ -28,7 +34,12 @@ func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
+	decoder := schema.NewDecoder()
+	var form SignUpForm
+	err = decoder.Decode(&form, r.PostForm)
+	if err != nil {
+		panic(err)
+	}
 
-	fmt.Fprintln(w, r.PostForm["email"])
-	fmt.Fprintln(w, r.PostFormValue("email"))
+	fmt.Fprintln(w, form)
 }
